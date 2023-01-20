84ff52f10d89aebd12366a61f96d978c998f1ad9
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-01-20 17:07:50 -0500

    Dimensions: Update with last changes

--- includes/type/dimensions.php
+++ includes/type/dimensions.php
@@ -1,6 +1,6 @@
 <h3>Dimensions</h3>
 
-<h4>Example</h4>
+<h4>Example linked values</h4>
 
 <div class="tangible-settings-row">
   <?= $fields->render_field($plugin->get_settings_key() . '[setting_dimensions_name]', [
@@ -8,12 +8,24 @@
     'type'        => 'dimensions',
     'description' => 'Description',
     'units'       => [ 'vh','%', 'vw' ],
-    'linked'      => false,
     'value'       => $plugin->get_settings()['setting_dimensions_name'] ?? '',
   ]) ?>
 </div>
 
-<h4>Example with linked values</h4>
+<h4>Example without linked values</h4>
+
+<div class="tangible-settings-row">
+  <?= $fields->render_field($plugin->get_settings_key() . '[setting_dimensions_name_not_linked_name]', [
+    'label'       => 'Dimensions field',
+    'type'        => 'dimensions',
+    'description' => 'Description',
+    'units'       => [ 'vh','%', 'vw' ],
+    'linked'      => false,
+    'value'       => $plugin->get_settings()['setting_dimensions_name_not_linked_name'] ?? '',
+  ]) ?>
+</div>
+
+<h4>Example with forced linked values</h4>
 
 <div class="tangible-settings-row">
   <?= $fields->render_field($plugin->get_settings_key() . '[setting_dimensions_linked_name]', [
@@ -34,6 +46,7 @@
 
 <?php tangible()->see(
   $plugin->get_settings()['setting_dimensions_name'] ?? '',
+  $plugin->get_settings()['setting_dimensions_name_not_linked_name'] ?? '',
   $plugin->get_settings()['setting_dimensions_linked_name'] ?? ''
 ); ?>
 
@@ -51,7 +64,7 @@
       'type'        => 'dimensions',
       'description' => 'Description',
       'units'       => [ 'px','vw' ], // Optional, default px
-      'linked'      => true, // Optional, default false
+      'linked'      => 'toggle', // Optional, default toggle (use true or false to force value)
       'value'       => $value,
     ]);
   </code> 
