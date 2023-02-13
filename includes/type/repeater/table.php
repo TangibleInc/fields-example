29b7ac159bb048debd1ea02a0fefa93b10b164b7
Reimar Rosas <reimar@reimarrosas.me>
2023-02-13 13:38:01 -0500

    Repeater: Added examples for `repeatable` and `maxlength` properties

--- includes/type/repeater/table.php
+++ includes/type/repeater/table.php
@@ -3,7 +3,7 @@
 
 A repeater row can contain any field.
 
-<h4>Example</h4>
+<h4>Example with Base Repeater Table</h4>
 
 <div class="tangible-settings-row">
   <?= $fields->render_field($plugin->get_settings_key() . '[setting_repeater_table_name]', [
@@ -48,11 +48,103 @@ A repeater row can contain any field.
   <?php submit_button() ?>
 </div>
 
+<h4>Example with `'repeatable' => false` property</h4>
+
+<div class="tangible-settings-row">
+  <?= $fields->render_field($plugin->get_settings_key() . '[setting_repeater_repeatable_table_name]', [
+    'label' => 'Repeater Repeatable Field',
+    'type' => 'repeater',
+    'layout' => 'table',
+    'repeatable' => false,
+    'value' => $plugin->get_settings()['setting_repeater_repeatable_table_name'] ?? false,
+    'sub_fields' => [
+      [
+        'label'   => 'Text',
+        'type'    => 'text',
+        'name'    => 'text_name',
+      ], [
+        'label'   => 'Dynamic text name',
+        'type'    => 'text_suggestion',
+        'name'    => 'dynamic_text_name',
+        'choices' => [
+          [
+            'name'    => 'Category 1',
+            'choices' => [
+              'test1' => 'Test1',
+              'test2' => 'Test2'
+            ]
+          ], [
+            'name'    => 'Category 2',
+            'choices' => [
+              'test3' => 'Test3',
+              'test4' => 'Test4'
+            ]
+          ]
+        ]
+      ], [
+        'label' => 'Date field',
+        'type'  => 'date_picker',
+        'name'  => 'date_picker_name',
+      ]
+    ]
+  ]) ?>
+</div>
+
+<div class="tangible-settings-row">
+  <?php submit_button() ?>
+</div>
+
+<h4>Example with `'maxlength' => 3` property</h4>
+
+<div class="tangible-settings-row">
+  <?= $fields->render_field($plugin->get_settings_key() . '[setting_repeater_maxlength_table_name]', [
+    'label' => 'Repeater Maxlength Field',
+    'type' => 'repeater',
+    'layout' => 'table',
+    'maxlength' => 3,
+    'value' => $plugin->get_settings()['setting_repeater_maxlength_table_name'] ?? false,
+    'sub_fields' => [
+      [
+        'label'   => 'Text',
+        'type'    => 'text',
+        'name'    => 'text_name',
+      ], [
+        'label'   => 'Dynamic text name',
+        'type'    => 'text_suggestion',
+        'name'    => 'dynamic_text_name',
+        'choices' => [
+          [
+            'name'    => 'Category 1',
+            'choices' => [
+              'test1' => 'Test1',
+              'test2' => 'Test2'
+            ]
+          ], [
+            'name'    => 'Category 2',
+            'choices' => [
+              'test3' => 'Test3',
+              'test4' => 'Test4'
+            ]
+          ]
+        ]
+      ], [
+        'label' => 'Date field',
+        'type'  => 'date_picker',
+        'name'  => 'date_picker_name',
+      ]
+    ]
+  ]) ?>
+</div>
+
+<div class="tangible-settings-row">
+  <?php submit_button() ?>
+</div>
+
 <h4>Value</h4>
 
-<?php tangible()->see(
-  $plugin->get_settings()['setting_repeater_table_name'] ?? ''
-); ?>
+<div><?php tangible()->see($plugin->get_settings()['setting_repeater_table_name'] ?? '') ?></div>
+<div><?php tangible()->see($plugin->get_settings()['setting_repeater_repeatable_table_name'] ?? '') ?></div>
+<div><?php tangible()->see($plugin->get_settings()['setting_repeater_maxlength_table_name'] ?? '') ?></div>
 
 <h4>Code</h4>
 
@@ -70,6 +162,10 @@ A repeater row can contain any field.
       'type'    => 'repeater',
       'layout'  => 'table',
       'value'   => $value,
+
+      // repeatable has higher priority than maxlength
+      'repeatable' => false, // If false, Disallows adding and removing of a single item
+      'maxlength' => 3, // Limits the number of items that can be added on the repeater
 
       // Can be any type of field, just make sure to add a name
 
