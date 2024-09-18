bca36fc8d48c895552cf298aac62b49c96e6ffb1
Adrian Buenavista <adrian@teamtangible.com>
2024-09-18 19:57:15 +0800

    update data

--- includes/templates/elements/tooltip.php
+++ includes/templates/elements/tooltip.php
@@ -1,43 +1,82 @@
-<h4>Example - Tooltip</h4>
+<h4>Example - Tooltip start</h4>
 
 <div class="tangible-settings-row">
-  <?php $fields->register_element('tooltip', [
-    'type'    => 'tooltip',
-    'layout'  => 'default',
-    'content' => 'Tooltip text'
+  <?php $fields->register_element('tooltip-start', [
+    'type'      => 'tooltip',
+    'label'     => 'Hover Start',
+    'placement' => 'start',
+    'content'   => 'Tooltip content, placement on start'
   ]) ?>
-  <?= $fields->render_element('tooltip') ?>
+  <?= $fields->render_element('tooltip-start') ?>
+</div>
+
+<h4>Example - Tooltip top</h4>
+
+<div class="tangible-settings-row">
+  <?php $fields->register_element('tooltip-top', [
+    'type'      => 'tooltip',
+    'label'     => 'Hover Top',
+    'placement' => 'top',
+    'content'   => 'Tooltip, placement on top'
+  ]) ?>
+  <?= $fields->render_element('tooltip-top') ?>
+</div>
+
+<h4>Example - Tooltip bottom</h4>
+
+<div class="tangible-settings-row">
+  <?php $fields->register_element('tooltip-bottom', [
+    'type'      => 'tooltip',
+    'label'     => 'Hover Bottom',
+    'placement' => 'bottom',
+    'content'   => 'Tooltip content, placement on bottom'
+  ]) ?>
+  <?= $fields->render_element('tooltip-bottom') ?>
+</div>
+
+<h4>Example - Tooltip end</h4>
+
+<div class="tangible-settings-row">
+  <?php $fields->register_element('tooltip-end', [
+    'type'      => 'tooltip',
+    'label'     => 'Hover End',
+    'placement' => 'end',
+    'content'   => 'Tooltip, placement on end'
+  ]) ?>
+  <?= $fields->render_element('tooltip-end') ?>
 </div>
 
 <h4>Code</h4>
 
 <div class="tangible-settings-code-preview">
 
-<?php $this->start_code('php') ?>
+  <?php $this->start_code('php') ?>
 
-// Example - PHP
+  // Example - PHP
 
-$fields->register_element('tooltip-name', [
-  'type'    => 'tooltip',
-  'layout'  => 'default', // Optional: 'default', 'primary'
-  'content' => 'Tooltip text'
-);
+  $fields->register_element('tooltip-name', [
+    'type'       => 'tooltip',
+    'label'      => 'Top'
+    'placement'  => 'top', // Optional: 'start', 'top', 'bottom', 'end' 
+    'content'    => 'Tooltip content, placement on top'
+  );
 
-echo $fields->render_element('tooltip-name')
+  echo $fields->render_element('tooltip-name')
 
-<?php $this->end_code() ?>
-<?php $this->start_code('javascript') ?>
+  <?php $this->end_code() ?>
+  <?php $this->start_code('javascript') ?>
 
-// Example - JS
+  // Example - JS
 
-const component = tangibleFields.render(
-  {
-    name    : 'tooltip-name',
-    type    : 'tooltip',
-    layout  : 'default'
-    content : 'Tooltip text'
-  }
-  'element'
-)
-<?php $this->end_code() ?>
+  const component = tangibleFields.render(
+    {
+      name       : 'tooltip-name',
+      type       : 'tooltip',
+      label      : 'Top',
+      placement  : 'top',
+      content    : 'Tooltip content, placement on top'
+    }
+    'element'
+  )
+  <?php $this->end_code() ?>
 </div>
